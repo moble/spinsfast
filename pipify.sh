c7214bfea8a0a613259ac9833100d9a77b22372e
Michael Boyle <michael.oliver.boyle@gmail.com>
2017-09-29 09:51:30 -0400

    Start making changes for pip compilation

---
+++ pipify.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+export datetime=$(date +"%Y.%m.%d.%H.%M.%S")
+python python/setup.py sdist bdist_wheel upload
+source activate py27
+python python/setup.py bdist_wheel upload
+
+open --hide --background -a Docker
+while ! (docker ps > /dev/null 2>&1); do
+    sleep 1
+done
+docker build -t pipify_spinsfast docker
+
