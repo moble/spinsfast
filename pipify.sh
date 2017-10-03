5b2d2a8f3c52ea96babaeec1da1de0fc41309bb2
Michael Boyle <michael.oliver.boyle@gmail.com>
2017-10-03 11:17:06 -0400

    Build manylinux wheels for pip distribution

--- pipify.sh
+++ pipify.sh
@@ -1,14 +1,23 @@
 #! /bin/bash
 
 export datetime=$(date +"%Y.%m.%d.%H.%M.%S")
-python python/setup.py sdist bdist_wheel upload
+python setup.py sdist bdist_wheel upload
 source activate py27
-python python/setup.py bdist_wheel upload
+python setup.py bdist_wheel upload
 source deactivate
 
-# open --hide --background -a Docker
-# while ! (docker ps > /dev/null 2>&1); do
-#     sleep 1
-# done
-# docker build -t pipify_spinsfast docker
+open --hide --background -a Docker
+while ! (docker ps > /dev/null 2>&1); do
+    sleep 1
+done
+docker run -i -t \
+    -v ${HOME}/.pypirc:/root/.pypirc:ro \
+    -v `pwd`:/code \
+    -v `pwd`/python/build_manylinux_wheels.sh:/build_manylinux_wheels.sh \
+    quay.io/pypa/manylinux1_x86_64 /build_manylinux_wheels.sh "${datetime}"
 
+
+# docker build -t pipify_spinsfast docker
+# docker run -v ${HOME}/.pypirc:/var/www/.pypirc:ro -v ${PWD}:/var/www/spinsfast -i -t pipify_spinsfast
+
+# cd /var/www/spinsfast && python setup.py bdist_wheel
