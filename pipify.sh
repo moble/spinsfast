f6e514aa55130c420806aa9d76b7eade352b614b
Michael Boyle <michael.oliver.boyle@gmail.com>
2017-10-05 14:34:15 -0400

    Build OSX wheels and use delocate to fix libraries

--- pipify.sh
+++ pipify.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
 export datetime=$(date +"%Y.%m.%d.%H.%M.%S")
-python setup.py sdist bdist_wheel upload
-source activate py27
-python setup.py bdist_wheel upload
-source deactivate
+
+python setup.py sdist upload
+
+./python/build_macosx_wheels.sh
 
 open --hide --background -a Docker
 while ! (docker ps > /dev/null 2>&1); do
+    echo "Waiting for docker to start..."
     sleep 1
 done
 docker run -i -t \
@@ -15,9 +16,3 @@ docker run -i -t \
     -v `pwd`:/code \
     -v `pwd`/python/build_manylinux_wheels.sh:/build_manylinux_wheels.sh \
     quay.io/pypa/manylinux1_x86_64 /build_manylinux_wheels.sh "${datetime}"
-
-
-# docker build -t pipify_spinsfast docker
-# docker run -v ${HOME}/.pypirc:/var/www/.pypirc:ro -v ${PWD}:/var/www/spinsfast -i -t pipify_spinsfast
-
-# cd /var/www/spinsfast && python setup.py bdist_wheel
