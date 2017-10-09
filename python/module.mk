c65c09e7243918d22d8104dbed45ba028a3fc29d
Mike Boyle <moble@users.noreply.github.com>
2017-10-09 17:30:13 -0400

    Update path to setup.py

--- python/module.mk
+++ python/module.mk
@@ -1,4 +1,4 @@
 LIB+=$(LIBDIR)/spinsfast.so
 
-$(LIBDIR)/spinsfast.so : python/spinsfast_module.c python/setup.py $(LIBDIR)/libspinsfast.a
-	python python/setup.py build --build-base $(OBJDIR) install --install-lib $(LIBDIR)
+$(LIBDIR)/spinsfast.so : python/spinsfast_module.c setup.py $(LIBDIR)/libspinsfast.a
+	python setup.py build --build-base $(OBJDIR) install --install-lib $(LIBDIR)
