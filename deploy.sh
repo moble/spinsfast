a4c45a1ff7e85d926269fba6bb910739c6053a5c
Michael Boyle <michael.oliver.boyle@gmail.com>
2017-10-12 15:29:49 -0400

    Simplify docker_miniconda

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,30 @@
 export datetime=$(date +"%Y.%m.%d.%H.%M.%S")
 export package_version="104.${datetime}"
 
+# Create a pure source pip package
 python setup.py sdist upload
 
+# Create all the osx binary pip packages
 ./python/build_macosx_wheels.sh
 
+# Create all the osx conda packages
+conda build .
+
+# Start docker for the linux packages
 open --hide --background -a Docker
 while ! (docker ps > /dev/null 2>&1); do
     echo "Waiting for docker to start..."
     sleep 1
 done
+
+# Create all the linux binary pip packages on centos 5
 docker run -i -t \
     -v ${HOME}/.pypirc:/root/.pypirc:ro \
     -v `pwd`:/code \
     -v `pwd`/python/build_manylinux_wheels.sh:/build_manylinux_wheels.sh \
     quay.io/pypa/manylinux1_x86_64 /build_manylinux_wheels.sh "${datetime}"
 
-conda build .
+# Create all the linux binary conda packages on centos 6
 docker build -t manyconda --pull python/docker_miniconda
 docker run -i -t \
     -e package_version \
--- python/docker_miniconda/build.sh
+++
@@ -1,9 +0,0 @@
-conda install -n root conda-build anaconda-client
-
-for PYTHON_VERSION in ${PYTHON_VERSIONS}; do
-    echo "Building with python ${PYTHON_VERSION}"
-    source activate "py${PYTHON_VERSION}"
-    cd /code
-    conda build .
-    source deactivate
-done
