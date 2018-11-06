e867a7b0c40c67639907b899a4d0a669ef4e3c13
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-11-06 09:51:59 -0500

    Remove leak-testing script

--- example/module.mk
+++ example/module.mk
@@ -1,4 +1,4 @@
-EXEC+=$(EXECDIR)/example_spin $(EXECDIR)/example_multispin $(EXECDIR)/example_multispin_looped $(EXECDIR)/example_iqu
+EXEC+=$(EXECDIR)/example_spin $(EXECDIR)/example_multispin $(EXECDIR)/example_iqu
 
 LIB_example:= -Llib -lspinsfast $(LIBFFTW) -lm
 
@@ -6,9 +6,6 @@ $(EXECDIR)/example_spin : example/example_spin.c lib/libspinsfast.a
 	$(CC) $(INC) -o $@ $< $(LIB_example)
 
 $(EXECDIR)/example_multispin : example/example_multispin.c lib/libspinsfast.a
-	$(CC) $(INC) -o $@ $< $(LIB_example)
-
-$(EXECDIR)/example_multispin_looped : example/example_multispin_looped.c lib/libspinsfast.a
 	$(CC) $(INC) -o $@ $< $(LIB_example)
 
 $(EXECDIR)/example_iqu : example/example_iqu.c lib/libspinsfast.a
