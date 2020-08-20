fde8fce7dadfd608db79eb40bec9577977153d58
Michael Boyle <michael.oliver.boyle@gmail.com>
2020-08-19 23:58:04 -0400

    Try C++

--- include/fftw.h
+++ include/fftw.h
@@ -1,6 +1,6 @@
 #if _MSC_VER && !__INTEL_COMPILER
-#include <complex.h>
-#define FFTW_NO_Complex
+#include <complex>
+/* #define FFTW_NO_Complex */
 #include <fftw3.h>
 /* #undef I */
 /* typedef double fftw_complex[2]; */
