997556e58ffc89eaf94251d4ec77ca6e5814a386
Michael Boyle <michael.oliver.boyle@gmail.com>
2022-02-08 12:52:48 -0500

    Skip older pythons

--- .github/workflows/actions/entrypoint.sh
+++ .github/workflows/actions/entrypoint.sh
@@ -4,7 +4,7 @@ yum install -y fftw3 fftw3-devel
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
-PYTHONS=("cp36-cp36m" "cp37-cp37m" "cp38-cp38" "cp39-cp39" "cp310-cp310")
+PYTHONS=("cp38-cp38" "cp39-cp39" "cp310-cp310")
 
 for PYTHON in ${PYTHONS[@]}; do
     /opt/python/${PYTHON}/bin/pip install --upgrade pip wheel auditwheel
